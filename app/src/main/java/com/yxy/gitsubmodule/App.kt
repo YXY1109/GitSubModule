c282947ab45b02eedbcd56c8ad2bfe0fe2d34bec
522917595@qq.com <chengjie1004>
2021-10-12 09:36:17 +0800

    准备介入rxhttp，但是编译一直未成功，稍后再试

---
+++ app/src/main/java/com/yxy/gitsubmodule/App.kt
@@ -0,0 +1,15 @@
+package com.yxy.gitsubmodule
+
+import android.app.Application
+
+class App : Application() {
+
+//    @DefaultDomain
+//    val baseUrl = "https://yangxunyu.xyz:8087"
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+}
