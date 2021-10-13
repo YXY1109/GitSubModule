696d2bc2c70a3ff2c84dab2b9afc55c11307cb8a
522917595@qq.com <chengjie1004>
2021-10-13 09:43:30 +0800

    简单自定义exo播放控制器

--- app/src/main/java/com/yxy/gitsubmodule/ui/activity/ExoActivity.kt
+++ app/src/main/java/com/yxy/gitsubmodule/ui/activity/ExoActivity.kt
@@ -2,9 +2,11 @@ package com.yxy.gitsubmodule.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.ui.PlayerView
@@ -15,6 +17,8 @@ import com.yxy.gitsubmodule.R
  * https://github.com/google/ExoPlayer
  * https://www.jianshu.com/p/cea336200990
  * https://blog.csdn.net/achina2011jy/article/details/112674293
+ *
+ * 官方文档翻译：https://blog.csdn.net/qq_35864421/article/details/115345091
  */
 class ExoActivity : AppCompatActivity() {
     private val videoPath = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"
@@ -60,18 +64,42 @@ class ExoActivity : AppCompatActivity() {
         if (playUri == null) {
             return
         }
-        val player = SimpleExoPlayer.Builder(this).build()
+//        val player = SimpleExoPlayer.Builder(this).build()
+//        player.playWhenReady = true
+//        player.repeatMode = Player.REPEAT_MODE_ALL
 
-        player.playWhenReady = true
-        player.repeatMode = Player.REPEAT_MODE_ALL
+        val player = SimpleExoPlayer.Builder(this).build().also {
+            it.playWhenReady = true
+            //视频播放模式
+            //it.repeatMode = Player.REPEAT_MODE_ALL
+        }
 
         val mediaItem = MediaItem.fromUri(playUri)
         player.addMediaItem(mediaItem)
-//        player.setMediaItem(mediaItem)
+        //player.setMediaItem(mediaItem)
 
         playerView?.player = player
 
         player.prepare()
-//        player.play()
+        //player.play()
+
+        player.addListener(object : Player.Listener {
+
+            override fun onPlaybackStateChanged(playbackState: Int) {
+                Log.i("yxyyxy", "playbackState:$playbackState")
+                when (playbackState) {
+                    Player.STATE_BUFFERING ->
+                        Log.i("yxyyxy", "加载中")
+                    Player.STATE_READY ->
+                        Log.i("yxyyxy", "准备完毕")
+                    Player.STATE_ENDED ->
+                        Log.i("yxyyxy", "播放完成")
+                }
+            }
+
+            override fun onPlayerError(error: PlaybackException) {
+                Log.i("yxyyxy", "onPlayerError：" + error.errorCodeName)
+            }
+        })
     }
 }
